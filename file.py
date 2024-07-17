70f11134a840ecff113839146155f14547f8b89a
Jônatas Wehrmann <jonataswehrmann@gmail.com>
2024-07-17 16:16:43 -0300

    Add more code

--- file.py
+++ file.py
@@ -1,5 +1,5 @@
-from typing import Literal
-from pydantic import BaseModel
+from typing import Literal, Optional
+from pydantic import BaseModel, Field
 
 
 class File(BaseModel):
@@ -8,3 +8,26 @@ class File(BaseModel):
     type: Literal["txt", "py"]
     path: str  # eg: /index.html
     status: Literal["new", "deleted"] = "new"
+
+
+class MeltingFaceEncoderArgs(BaseModel):
+    model: str
+    batch_size: int = Field(default=128)
+    dims: Optional[int] = None
+
+    def __hash__(self) -> int:
+        return f"{self.model}{self.dims}".__hash__()
+
+
+class HouseRegister(BaseModel):
+    size: int
+    price: int
+    address: str
+    description: str
+    image: str
+    status: Literal["new", "deleted"] = "new"
+
+
+class HousePriceEstimatorPlugin():
+    def predict(self, house_register: HouseRegister) -> int:
+        raise NotImplementedError
